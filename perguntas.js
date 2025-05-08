b3589d47b2964b357ebbb5c47edf7003cc42b026
shallaala <164891739+shallaala@users.noreply.github.com>
2025-05-08 08:00:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Bloodstrike',
+    'O que é slide lateral?',
+    'O slide lateral é um dash para o lado'
+)
+
+criaCartao(
+    'God of War',
+    'Qual o primeiro Deus que traiu kratos?',
+    'O primeiro Deus que traiu Kratos foi o Ares'
+)
+
+criaCartao(
+    'Mortal Kombat',
+    'quem é o sensei de Liu Kang e Kung Lao?',
+    'O sensei de ambos é o Raiden'
+)
+
+criaCartao(
+    'Guitar Hero',
+    'quais são as teclas no controle para cada cor da guitarra?',
+    'As teclas são R2 (para o azul) L2 (para o verde) R1 (para o vermelho) L1 (para o amarelo) e X (para o laranja)'
+)
